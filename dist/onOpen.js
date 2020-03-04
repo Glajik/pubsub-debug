c3e6eb6b349b68202d42b98a7c076cadee2fcb7b
Viacheslav Glazhevskyi <glajik@gmail.com>
2020-03-05 01:08:34 +0200

    A time trigger is used to continue pulling messages if they are in the queue.

--- dist/onOpen.js
+++ dist/onOpen.js
@@ -1,7 +1,5 @@
-/* globals SpreadsheetApp : true */
-import R from 'ramda';
-import pullMessages from './pubsub/pullMessages';
-import ackMessages from './pubsub/ackMessages';
+/* globals SpreadsheetApp ScriptApp : true */
+import pullMessagesForSheet from './pullMessagesForSheet';
 
 // eslint-disable-next-line no-unused-vars
 function onOpen() {
@@ -11,72 +9,25 @@ function onOpen() {
     .addToUi();
 }
 
+function byTimeTrigger() {
+  const ss = SpreadsheetApp.getActiveSpreadsheet();
+  const sheets = ss.getSheets();
+  const results = sheets.map(pullMessagesForSheet);
+
+  // Remove trigger if no messages received for every sheet
+  if (results.every(count => count === 0)) {
+    ScriptApp.getProjectTriggers().forEach(
+      trigger => ScriptApp.deleteTrigger(trigger)
+    );
+  }
+}
+
 // eslint-disable-next-line no-unused-vars
-
-/**
- * @typedef Message
- * @type {object}
- * @property {string} data
- * @property {object} attributes
- * @property {string} messageId
- * @property {string} publishTime
- */
-
-/**
- * @typedef ReceivedMessage
- * @type {object}
- * @property {string} ackId
- * @property {Message} message
- */
-
 function handleMenuItemPullMessages() {
-  const sheet = SpreadsheetApp.getActiveSheet();
-  const sheetName = sheet.getSheetName();
-  const subscription = `projects/autocrash-erpnext/subscriptions/${sheetName}`;
+  byTimeTrigger();
 
-  // Pull messages
-  const receivedMessages = pullMessages(subscription);
-
-  /**
-   * Convert message to rowContent
-   * @param {Message} message 
-   */
-  const toRowContent = (message) => {
-    const {
-      data, attributes, messageId, publishTime
-    } = message;
-
-    const {
-      type, payload, error, ...rest
-    } = data;
-
-    const main = [messageId, new Date(publishTime), attributes];
-
-    if (!type) {
-      return [...main, '-', JSON.stringify(data), ''];
-    }
-
-    if (error) {
-      return [...main, type, error.message, ''];
-    }
-
-    return [...main, type, JSON.stringify(payload), JSON.stringify(rest)];
-  };
-
-  // Get messages from list
-  const messages = receivedMessages.map(R.prop('message'));
-
-  // Convert to spreadsheet values
-  const values = messages.map(toRowContent);
-
-  // Get range and set values
-  const row = sheet.getLastRow() + 1;
-  const col = 1;
-  const numRows = values.length;
-  const numColumns = 6;
-  sheet.getRange(row, col, numRows, numColumns).setValues(values);
-
-  // Ack messages by ackId
-  const ackIds = receivedMessages.map(R.prop('ackId'));
-  ackMessages(subscription, ackIds);
+  ScriptApp.newTrigger('byTimeTrigger')
+    .timeBased()
+    .everyMinutes(1)
+    .create();
 }
---
+++ dist/pullMessagesForSheet.js
@@ -0,0 +1,76 @@
+import R from 'ramda';
+import pullMessages from './pubsub/pullMessages';
+import ackMessages from './pubsub/ackMessages';
+
+/**
+ * @typedef Message
+ * @type {object}
+ * @property {string} data
+ * @property {object} attributes
+ * @property {string} messageId
+ * @property {string} publishTime
+ */
+
+/**
+ * @typedef ReceivedMessage
+ * @type {object}
+ * @property {string} ackId
+ * @property {Message} message
+ */
+
+/**
+ * Convert message to rowContent
+ * @param {Message} message
+ */
+const messageToRowContent = (message) => {
+  const {
+    data, attributes, messageId, publishTime
+  } = message;
+
+  const {
+    type, payload, error, ...rest
+  } = data;
+
+  const main = [messageId, new Date(publishTime), JSON.stringify(attributes)];
+
+  if (!type) {
+    return [...main, '-', JSON.stringify(data), ''];
+  }
+
+  if (error) {
+    return [...main, type, error.message, ''];
+  }
+
+  return [...main, type, JSON.stringify(payload), JSON.stringify(rest)];
+};
+
+function pullMessagesForSheet(sheet) {
+  const subscription = `projects/autocrash-erpnext/subscriptions/${sheet.getName()}`;
+
+  const receivedMessages = pullMessages(subscription);
+
+  if (R.isEmpty(receivedMessages)) {
+    return 0;
+  }
+
+  // Get messages from list
+  const messages = receivedMessages.map(R.prop('message'));
+
+  // Convert to spreadsheet values
+  const values = messages.map(messageToRowContent);
+
+  // Get range and set values
+  const row = sheet.getLastRow() + 1;
+  const col = 1;
+  const numRows = values.length;
+  const numColumns = 6;
+  sheet.getRange(row, col, numRows, numColumns).setValues(values);
+
+  // Ack messages by ackId
+  const ackIds = receivedMessages.map(R.prop('ackId'));
+  ackMessages(subscription, ackIds);
+
+  return receivedMessages.length;
+}
+
+export default pullMessagesForSheet;
